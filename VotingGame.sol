59a1346e6c4562b8331e15330c66ba1213fa5def
maya-maye <95601135+maya-maye@users.noreply.github.com>
2022-05-17 15:52:11 -0700

    Update VotingGame.sol

--- VotingGame.sol
+++ VotingGame.sol
@@ -1,1 +1,46 @@
+pragma solidity 0.8.11;
 
+contract VotingGame{
+    address public owner;
+    mapping (address => uint) public Balances;
+    uint poolA;
+    uint poolB;
+    address[] a;
+    address[] b;
+    uint[] increAmountA;
+    uint[] increAmountB;
+    
+    constructor(){
+        owner=msg.sender;
+        Balances[address(this)]=0;
+        choiceA=0;
+        choiceB=0;
+    }
+
+    function getBalance() public view returns (uint){
+        return Balances[address(this)];
+    }
+
+    function getMoneyInPoolA() returns(uint){
+        return poolA;
+    }
+
+    function getMoneyInPoolB() returns(uint){
+        return poolB;
+    }
+
+    //decrease the user's money by amount and increase ours. 
+    //can't use private?
+    function putInA(uint amount) public payable{
+        poolA+=amount;
+        Balances[address(this)]+=amount;
+        Balances[msg.sender]-=amount;
+    }
+    //same function for B
+
+    //gives the winner back their money doubled
+    function giveBack(uint amount) public payable{
+
+    }
+
+}
